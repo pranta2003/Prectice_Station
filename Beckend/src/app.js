a3c47c6cc712acc8ac3868f099b3bdffef636dcf
Pranta <prantakumerpandit@gmail.com>
2026-03-19 13:13:27 +0600

    route declare

--- Beckend/src/app.js
+++ Beckend/src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
 
 const app = express(); // creating 
+// import routes 
+import userRouter from "./routes/user.routes.js";
+
+// declare routes
+app.use("api/v1/users", userRouter);
+
 
 
 
