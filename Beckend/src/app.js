9e60b6b6615d40dcec5eaaa7a354b5cba7545b37
Pranta <prantakumerpandit@gmail.com>
2026-03-19 13:34:12 +0600

    making register route

--- Beckend/src/app.js
+++ Beckend/src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 
 const app = express(); // creating 
+
+app.use(express.json()); //to parse json data from request body . ok nigga ?
+
+
+
 // import routes 
 import userRouter from "./routes/user.routes.js";
 
 // declare routes
 app.use("api/v1/users", userRouter);
 
+// example route : https://localhost:4000/api/v1/users/registers
 
 
-
+ 
 export default app;
