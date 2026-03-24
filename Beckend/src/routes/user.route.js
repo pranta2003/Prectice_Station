963497582d01ddd2adf2a58ae7978f320c2be779
Pranta <prantakumerpandit@gmail.com>
2026-03-24 14:37:14 +0600

    loginUSer route adding

--- Beckend/src/routes/user.route.js
+++ Beckend/src/routes/user.route.js
@@ -3,6 +3,7 @@ import { registerUser } from "../controllers/user.controller.js";
 const router = Router();
 
 router.route('/register').post( registerUser);
+router.route('/login').post( loginUser);
 
 
 export default router;
