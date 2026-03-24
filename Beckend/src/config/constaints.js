5aa53dea4d8059f6fb87805cce1aeeb3ecba39a5
Pranta <prantakumerpandit@gmail.com>
2026-03-24 14:17:21 +0600

    unserstanding async , await comapre with .then() ,  .catch()

--- Beckend/src/config/constaints.js
+++ Beckend/src/config/constaints.js
@@ -1,2 +1,9 @@
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const DB_NAME = "Precrtice_working";
+
+
+// setTimeout(() => {
+//     console.log("lol");
+// },2000);
+
+// function my() { setTimeout(() => { console.log("h"); }, 2000); }
